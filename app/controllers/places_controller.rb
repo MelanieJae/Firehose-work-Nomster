592cbf0f9fcfc6e68d43039bf1087ae745124677
melaniejae <mtaurus575@gmail.com>
2015-04-10 15:51:25 +0000

    Cleaned up the sign in form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,7 +7,7 @@ class PlacesController < ApplicationController
 
 	def create
 		current_user.places.create(place_params)
-		Place.create(place_params)
+		#Place.create(place_params)
 		redirect_to root_path	
 	end
 
