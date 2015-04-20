1f93cba427342a0b7148ccbdd81b024d4be9623a
MelanieJae <mtaurus575@gmail.com>
2015-04-20 00:25:19 +0000

    adding create comment form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Nomster::Application.routes.draw do
   devise_for :users
-  resources :places
   root to: 'places#index'
+  resources :places do
+    resources :comments, :only => :create
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
