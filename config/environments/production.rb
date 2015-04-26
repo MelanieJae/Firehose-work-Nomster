96a38478517b2c811641c3b8d0e05465abad8719
MelanieJae <mtaurus575@gmail.com>
2015-04-26 22:30:47 +0000

    Added mailgun for heroku version of app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,18 @@
 Nomster::Application.configure do
-  config.action_mailer.default_url_options = { :host => 'nomster-m-hand.herokuapp.com'}
-  # Settings specified here will take precedence over those in config/application.rb.
+  
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
 
-  # Code is not reloaded between requests.
+  ActionMailer::Base.delivery_method = :smtp
+ 
+  config.action_mailer.default_url_options = { :host => 'nomster-m-hand.herokuapp.com'}
+  # Settings specified here will take precedence over those in config/application.rb.# Code is not reloaded between requests.
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
