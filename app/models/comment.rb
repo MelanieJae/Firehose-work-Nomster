b8eb1f0d4cbe58994e47ef3e39d757769325caf0
MelanieJae <mtaurus575@gmail.com>
2015-05-19 01:28:28 +0000

    image uploader

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
-#	after_create :send_comment_email
+	after_create :send_comment_email
 
 	RATINGS = {
 		'1 star' => '1_star',
@@ -16,8 +16,8 @@ class Comment < ActiveRecord::Base
 		RATINGS.invert[self.rating]
 	end
 
-# 	def send_comment_email
-# 		NotificationMailer.comment_added(self).deliver
-# 	end
+ 	def send_comment_email
+ 		NotificationMailer.comment_added(self).deliver
+ 	end
 end
 
